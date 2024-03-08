bb13719604a7310734241ff8d4858b09d7b51356
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-03-08 16:19:33 +0530

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.listen(3000, () => {
   console.log(`app is listening at 3000`);
 });
